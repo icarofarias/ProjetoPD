f1d3eb2e4cd5641ea6edfd8029a21ed7ae0eaf43
icarofarias <ifdg.ele@uea.edu.br>
2021-05-09 13:01:47 -0400

    Projeto enviado

---
+++ WebServicop/WebServicop/WebService1.asmx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServicop
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+        [WebMethod]
+        public string MeuMetodo(int opcao)
+        {
+            if (opcao == 1)
+            {
+                return "Retorno do Metodo 1";
+            }
+            else if (opcao == 2)
+            {
+                return "Retorno do metodo 2";
+            }
+            else
+            {
+                return "retorno do metodo 3";
+            }
+
+        }
+    }
+}
